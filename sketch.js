8acd896a97471229863a328d2682fd4825a92831
SANCx3 <156867743+SANCx3@users.noreply.github.com>
2024-03-26 16:41:57 -0700

    Added Main Project Files
    Added the main p5,js files for da sound project(Pretty cool if ya ask me )

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+
+function setup() {
+  createCanvas(600, 400);
+    background(220);
+  strokeWeight(4);
+}
+
+
+function draw() {
+  
+  if (mouseIsPressed){
+    ellipse(mouseX,mouseY,10);
+    stroke(1)
+    
+  } else if(keyIsPressed){
+    rect(mouseX,mouseY,12,12);
+    stroke(random(255),random(132),random(245));
+  }
+  
+   rect(2,300,700,900);
+ rect(2,1,120,300);
+fill(1)
+}
